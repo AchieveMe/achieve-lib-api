117fd1da94360d5e1243733191c20d951310dce9
Guilherme Magalhaes <guiandmag@gmail.com>
2015-06-25 22:52:06 -0300

    fix(Test) : Change GeneratorSuperTest
    Refactor GeneratorSuperTest
    
    Closes #NoIssue

--- achieve-api-commons/src/test/java/vc/achieve/api/commons/generator/GeneratorSuperTest.java
+++ achieve-api-commons/src/test/java/vc/achieve/api/commons/generator/GeneratorSuperTest.java
@@ -1,17 +1,13 @@
 package vc.achieve.api.commons.generator;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-
 import vc.achieve.api.commons.generator.Generator;
 
 /**
  * @author GAN
  *
  */
-@RunWith(JUnit4.class)
 public class GeneratorSuperTest {
 	
 	protected Generator generator;
-
+	
 }
